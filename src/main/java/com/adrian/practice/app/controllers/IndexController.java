b32d32bdac1541e61605ca6eca0cb5d03f75d85c
Adrian Sanchez A <adriansa1997@gmail.com>
2025-08-30 13:22:08 -0700

    Add default endpoint

--- src/main/java/com/adrian/practice/app/controllers/IndexController.java
+++ src/main/java/com/adrian/practice/app/controllers/IndexController.java
@@ -1,8 +1,16 @@
 package com.adrian.practice.app.controllers;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-
+    
+    @GetMapping("/")
+    public ResponseEntity<Map<String, String>> index(){
+        return ResponseEntity.ok(Map.of("message", "Hello World"));
+    }
 }
