30d85a0f377fdbddf3731bc9d966320b8dc687f0
Adrian Sanchez A <adriansa1997@gmail.com>
2025-08-31 11:37:52 -0700

    Add comments

--- src/main/java/com/adrian/practice/app/components/HandlerError/HandlerController.java
+++ src/main/java/com/adrian/practice/app/components/HandlerError/HandlerController.java
@@ -8,41 +8,89 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.Getter;
 
+/**
+ * Class to handle Exceptions globally in the application.
+ * It uses @RestControllerAdvice to intercept exceptions thrown by controllers
+ * and return appropriate HTTP responses.
+ * It handles custom HttpException, NoHandlerFoundException, and common runtime exceptions.
+ * It constructs a standardized error response with error details.
+ * The ErrorResponse class is a static inner class that represents the structure of the error response.
+ * It includes fields for error message, detailed message, and HTTP status code.
+ * The ErrorResponse class has a static factory method to create instances based on HttpStatus and Exception
+ * details.
+ * @author Adrian SA
+ * @version 1.0
+ * @since 2025-08
+ */
 @RestControllerAdvice
 public class HandlerController {
-
-     @ExceptionHandler(HttpException.class)
-    public ResponseEntity<? > httpExceptionhandle(HttpException ex) {
+    /**
+     * Handles custom HttpException and constructs a ResponseEntity of ErrorResponse.
+     * @param HttpException 
+     * @return ResponseEntity<ErrorResponse> 
+     */
+    @ExceptionHandler(HttpException.class)
+    public ResponseEntity<?> httpExceptionhandle(HttpException ex) {
         HttpStatus status = ex.getHttpError();
-        return ResponseEntity.status(status).body(ErrorResponse.create(status, ex.getMessage()));
+        return ResponseEntity.status(status).body(ErrorResponse.create(status, ex));
     }
 
+    /**
+     * Handles NoHandlerFoundException and constructs a ResponseEntity of ErrorResponse.
+     * @param NoHandlerFoundException
+     * @return ResponseEntity<ErrorResponse>
+     */
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<?> noHandlerExceptionFound(NoHandlerFoundException ex) {
-        ErrorResponse response = ErrorResponse.create(HttpStatus.NOT_FOUND, ex.getMessage());
+        ErrorResponse response = ErrorResponse.create(HttpStatus.NOT_FOUND, ex);
         return ResponseEntity.status(ex.getStatusCode()).body(response);
     }
 
+    /**
+     * Handles common runtime exceptEions like NullPointerException and IllegalArgumentException.
+     * Constructs a ResponseEntity of ErrorResponse with HTTP status 500 (Internal Server Error).
+     * @param Exception ex
+     * @return ResponseEntity<ErrorResponse>
+     */
     @ExceptionHandler({NullPointerException.class, IllegalArgumentException.class})
     public ResponseEntity<?> runtimeExceptionHandle(Exception ex) {
-        ErrorResponse response = ErrorResponse.create(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        ErrorResponse response = ErrorResponse.create(HttpStatus.INTERNAL_SERVER_ERROR, ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
+    /**
+     * Static inner class representing the structure of the error response.
+     * It includes fields for error message, detailed message, and HTTP status code.
+     * @author Adrian SA
+     * @version 1.0
+     * @since 2025-08
+     */
     @Getter
     private static class ErrorResponse{
         private final String error;
         private final String message;
         private final int code;
 
+        /**
+         * Private constructor to initialize ErrorResponse fields.
+         * @param String error
+         * @param String message
+         * @param int code
+         */
         private ErrorResponse(String error, String message, int code) {
             this.error = error;
             this.message = message;
             this.code = code;
         }
 
-        public static ErrorResponse create(HttpStatus status, String message) {
-            return new ErrorResponse(status.getReasonPhrase(), message, status.value());
+        /**
+         * Static factory method to create an ErrorResponse instance based on HttpStatus and Exception details.
+         * @param HttpStatus status
+         * @param Exception ex
+         * @return ErrorResponse
+         */
+        public static ErrorResponse create(HttpStatus status, Exception ex) {
+            return new ErrorResponse(status.getReasonPhrase(), ex.getMessage(), status.value());
         }
 
     }
