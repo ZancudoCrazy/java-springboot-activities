d48152ef5096216243df1fddce149f814d959382
Adrian Sanchez A <adriansa1997@gmail.com>
2025-09-04 16:39:08 -0700

    Add security package and comments

--- src/main/java/com/adrian/practice/app/AppApplication.java
+++ src/main/java/com/adrian/practice/app/AppApplication.java
@@ -3,6 +3,12 @@ package com.adrian.practice.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class of the Spring Boot application.
+ * It contains the entry point of the application.
+ * It is annotated with @SpringBootApplication to enable
+ * component scanning, auto-configuration, and property support.
+ */
 @SpringBootApplication
 public class AppApplication {
 
